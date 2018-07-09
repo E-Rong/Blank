0c5b189b28c5fc8b510bf94779bc24fa6e9383a0
E-Rong Yong <e-rongyong@yong-e-rong.local>
2018-07-09 15:33:10 +0800

    Initial Commit

---
+++ Blank/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Blank
+//
+//  Created by E-Rong Yong on 9/7/18.
+//  Copyright © 2018 E-Rong Yong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
